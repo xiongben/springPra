79ce23fd40cf0e97d23705665b8eeda9d15f138e
xiongben <xiongben1993@gmail.com>
2019-11-19 11:56:35 +0800

    [add]注入

--- src/main/java/com/xiongben/service/impl/AccountServiceImpl.java
+++ src/main/java/com/xiongben/service/impl/AccountServiceImpl.java
@@ -5,15 +5,23 @@ import com.xiongben.dao.impl.AccountDaoImpl;
 import com.xiongben.factory.BeanFactory;
 import com.xiongben.service.IAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
 import java.util.Date;
 
 @Service("accountService")
+//@Scope("prototype")
 public class AccountServiceImpl implements IAccountService {
 //    private IAccountDao accountdao = (IAccountDao) BeanFactory.getBean("accountDao");
 
-    @Autowired
+//    @Autowired
+//    @Qualifier("accountDao1")
+    @Resource(name = "accountDao1")
     private IAccountDao accountdao;
 
 
@@ -29,4 +37,14 @@ public class AccountServiceImpl implements IAccountService {
 //    public void destroy(){
 //        System.out.println("对象销毁了，，，，，");
 //    }
+
+    @PostConstruct
+    public void  init(){
+        System.out.println("初始化方法执行了");
+    }
+
+    @PreDestroy
+    public void  destroy(){
+        System.out.println("销毁方法执行了");
+    }
 }
